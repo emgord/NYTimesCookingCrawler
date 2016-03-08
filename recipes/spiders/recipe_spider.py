215fd954a5fe9008b006c55da9e703025060d522
emgord <emgord@gmail.com>
2016-03-08 15:48:30 -0800

    recipe spider is working for one recipe, handles missing data without errors

--- recipes/spiders/recipe_spider.py
+++ recipes/spiders/recipe_spider.py
@@ -6,18 +6,32 @@ class RecipeSpider(scrapy.Spider):
     name = "recipe"
     allowed_domains = ["http://cooking.nytimes.com/"]
     start_urls = [
-        "http://cooking.nytimes.com/recipes/12790"
+        "http://cooking.nytimes.com/recipes/100"
     ]
 
     def parse(self, response):
         recipe = RecipeItem()
-        recipe['title'] = response.xpath('/html/body/div[2]/div/div[1]/article/header/h1/text()').extract()[0].strip()
-        recipe['image'] = response.xpath('/html/body/div[2]/div/div[1]/article/div[1]/div[1]/img/@src').extract()[0]
-        recipe['recipe_yield'] = response.xpath('/html/body/div[2]/div/div[1]/article/header/div[1]/ul/li[2]/span[2]/text()').extract()[0].strip()
-        recipe['time'] = response.xpath('/html/body/div[2]/div/div[1]/article/header/div[1]/ul/li[1]/text()').extract()[2].strip()
-        recipe['original_url'] = response.xpath('/html/body/div[2]/div/div[1]/article/@data-url').extract()[0]
-        recipe['description'] = response.xpath('/html/body/div[2]/div/div[1]/article/div[1]/div[2]/div/p[1]/text()').extract()[0].strip()
-        recipe['uid'] = response.xpath('/html/body/div[2]/div/div[1]/article/@data-id').extract()[0]
+        title = response.xpath('//h1[@itemprop="name"]/@data-name').extract()
+        if len(title) > 0:
+            recipe['title'] = title[0].strip()
+        image = response.xpath('//img[@itemprop="image"]/@src').extract()
+        if len(image) > 0:
+            recipe['image'] = image[0]
+        recipe_yield = response.xpath('//span[@itemprop="recipeYield"]/text()').extract()
+        if len(recipe_yield) > 0:
+            recipe['recipe_yield'] = recipe_yield[0].strip()
+        time = response.xpath('//ul[@class="recipe-time-yield"]/li[1]/text()').extract()
+        if len(time) > 2:
+            recipe['time'] = time[2].strip()
+        original_url = response.xpath('//article[@data-analytics-category="Recipe Detail Full"]/@data-url').extract()
+        if len(original_url) > 0:
+            recipe['original_url'] = original_url[0]
+        uid = response.xpath('//article[@data-analytics-category="Recipe Detail Full"]/@data-id').extract()
+        if len(uid) > 0:
+            recipe['uid'] = uid[0]
+        description = response.xpath('//div[@itemprop="description"]/p[1]/text()').extract()
+        if len(description) > 0:
+            recipe['description'] = description[0].strip()
         recipe['ingredients'] = []
         for ingredient in response.xpath('//li[@itemprop="recipeIngredient"]'):
             name = ingredient.xpath('span[@class="ingredient-name"]/span/text()').extract()
